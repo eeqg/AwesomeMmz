40b141c0075ce61508577538bdef493f5e233227
eeqg <876583632@qq.com>
2019-02-27 14:48:52 +0800

    +shadow

--- app/src/main/java/com/example/wp/awesomemmz/image/ImageActivity.java
+++ app/src/main/java/com/example/wp/awesomemmz/image/ImageActivity.java
@@ -1,18 +1,40 @@
 package com.example.wp.awesomemmz.image;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.wp.awesomemmz.R;
 import com.example.wp.resource.base.BaseActivity;
+import com.example.wp.resource.widget.ShadowDrawable;
+import com.example.wp.resource.widget.shadow.ShadowDrawableWrapper;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ImageActivity extends BaseActivity {
+	
+	@BindView(R.id.ivShadow)
+	View ivShadow;
+	@BindView(R.id.ivShadow3)
+	View ivShadow3;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_image);
-		
+		ButterKnife.bind(this);
 		setupTitleBar("Image");
+		
+		observeViewShadow();
+	}
+	
+	private void observeViewShadow() {
+		ShadowDrawable.setShadowDrawable(ivShadow, Color.parseColor("#801122FF"), 50,
+				Color.parseColor("#88FE5891"), 50, 0, 0);
+		
+		ShadowDrawableWrapper shadowDrawableWrapper = new ShadowDrawableWrapper(this,
+				getResources().getDrawable(R.mipmap.image4), 10, 15, 15);
+		ivShadow3.setBackgroundDrawable(shadowDrawableWrapper);
 	}
 }
