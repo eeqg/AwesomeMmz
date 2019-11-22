1e9c784e2d9c21162537011f638ca5c8f1450c27
eeqg <876583632@qq.com>
2019-11-22 15:46:14 +0800

    update..

--- app/src/main/java/com/example/wp/awesomemmz/frame/LifecycleActivity.java
+++ app/src/main/java/com/example/wp/awesomemmz/frame/LifecycleActivity.java
@@ -1,7 +1,9 @@
 package com.example.wp.awesomemmz.frame;
 
+import android.arch.lifecycle.GenericLifecycleObserver;
 import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.OnLifecycleEvent;
 import android.os.SystemClock;
 import android.os.Bundle;
@@ -22,11 +24,25 @@ public class LifecycleActivity extends AppCompatActivity {
 
         //AppCompatActivity和Fragment都实现了LifecycleOwner接口（Support Library 26.1.0之后的版本），所以可以直接拿来使用.
         //普通Activity类并没有实现LifecycleOwner接口, 要自己实现
+        //T1
         getLifecycle().addObserver(new TestLifeCycle());
+        //T2
         getLifecycle().addObserver(new LifecycleObserver() {
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            public void onResume() {
+                Log.d("--", "------onResume() ");
+            }
+
             @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
             public void onDestroy() {
-                Log.d("", "------onDestroy: ");
+                Log.d("--", "------onDestroy() ");
+            }
+        });
+        //T3
+        getLifecycle().addObserver(new GenericLifecycleObserver() {
+            @Override
+            public void onStateChanged(LifecycleOwner source, Lifecycle.Event event) {
+                Log.d("--", "------onStateChanged() -- "+ event);
             }
         });
 
